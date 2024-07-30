d81a751c62eb4ef3c66183ddcf53ffd09e49274c
Dennis Njau <njaudennis30@gmail.com>
2024-07-30 13:44:09 +0300

    updates routes.py

--- server/app.py
+++ server/app.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 
-# Standard library imports
-
 # Remote library imports
 from flask import request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
+from routes import auth_bp, expense_bp, budget_bp, goal_bp
 
-
-# Views go here!
+# Register blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(expense_bp, url_prefix='/expenses')
+app.register_blueprint(budget_bp, url_prefix='/budgets')
+app.register_blueprint(goal_bp, url_prefix='/goals')
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
