3662afcccbedfce06c6484ecc3cb8484a2636503
desmachebet <desmachebet@gmail.com>
2024-08-15 18:54:47 +0300

    adding styling

--- client/FinTrack/src/components/Home.jsx
+++ client/FinTrack/src/components/Home.jsx
@@ -3,18 +3,21 @@ import "./home.css";
 const Home = () => {
     return (
         <div className="full-height-container">
-            <h1 className="tittle">FinTrack</h1>
+            <div className="tittle">
+                <h1>FinTrack</h1>
+                <p>Your trusted financial companion</p>
+            </div>
             <div className="cards-container">
                 <div className="card">
-                    <h2>Expenses</h2>
+                    <h2> Track Expenses</h2>
                     <p>Track and manage your daily expenses effortlessly.</p>
                 </div>
                 <div className="card">
-                    <h2>Budgets</h2>
+                    <h2> Manage Budgets</h2>
                     <p>Create and monitor your budgets to stay on top of your finances.</p>
                 </div>
                 <div className="card">
-                    <h2>Insights</h2>
+                    <h2> Get financial Insights</h2>
                     <p>Gain insights into your spending patterns with visual data.</p>
                 </div>
             </div>
